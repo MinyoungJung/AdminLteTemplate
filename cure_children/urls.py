b97b49123001a0eb085799a10bdef7895270c029
MinyoungJung <minyoungjung84@gmail.com>
2017-07-18 23:27:12 +0900

    updated signup page

--- cure_children/urls.py
+++ cure_children/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from cure_children.views import HomeView, UserCreateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),   # Todo: 나중에 disable
 
     url(r'^', include('django.contrib.auth.urls')),
     url(r'register/$', UserCreateView.as_view(), name='register'),
+
+    url(r'^login/$', auth_views.login, {'template_name': 'home.html'}),
 ]
--- cure_children/views.py
+++ cure_children/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.forms import UserCreationForm
 
 
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin, TemplateView):
+    login_url = '/login'
+    redirect_field_name = 'redirect_to'
     template_name = 'home.html'
 
 
